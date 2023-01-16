6747348486d1415b39b03aa77a18bac1f0c0fa4d
SyMind <dacongsama@live.com>
2023-01-16 09:12:03 +0800

    add example

---
+++ example/src/app.js
@@ -0,0 +1,5 @@
+const app = {
+
+}
+
+export default app
---
+++ example/src/pages/index/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  function handleTap() {
+    count += 1;
+  }
+</script>
+
+<taro-button class="btn" on:tap={handleTap}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</taro-button>
+
+<style>
+.btn {
+  background-color: red;
+}
+</style>
