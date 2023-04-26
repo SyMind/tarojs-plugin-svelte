2e5b16f29912bfd1e44952960e0216cd2c95ced6
SyMind <dacongsama@live.com>
2023-04-26 08:29:36 +0800

    chore: add unit test

---
+++ test/loader.spec.js
@@ -0,0 +1,93 @@
+const taroSvelteLoader = require('../lib/taroSvelteLoader')
+
+jest.mock('@tarojs/webpack5-runner/dist/utils/component', () => ({
+    componentConfig: {
+        includes: new Set()
+    }
+}))
+
+function testLoader(source, callback, query, version = 2) {
+    return done => {
+        const addedDependencies = new Set();
+
+        function cb(...args) {
+            while (args.length < 4) {
+                args.push(undefined);
+            }
+            args.push(addedDependencies);
+            try {
+                callback(...args);
+            } catch (err) {
+                expect(callbackSpy).toBeCalled();
+                return done(err);
+            }
+            expect(callbackSpy).toBeCalled();
+            done();
+        }
+
+        const cacheableSpy = jest.fn();
+
+        const callbackSpy = jest.fn(cb);
+
+        const dependencySpy = jest.fn(function(p) { addedDependencies.add(p); });
+
+        taroSvelteLoader.call(
+            {
+                cacheable: cacheableSpy,
+                async: () => callbackSpy,
+                addDependency: dependencySpy,
+                resourcePath: '<nil>',
+                version,
+                query
+            },
+            source,
+            null
+        );
+
+        expect(cacheableSpy).toBeCalled();
+
+        for (const call of dependencySpy.mock.calls) {
+            expect(typeof call[0]).toBe('string')
+        }
+    };
+}
+
+describe('mini program', () => {
+    beforeAll(() => {
+        process.env.TARO_ENV = 'weapp'
+    })
+
+    afterAll(() => {
+        process.env.TARO_ENV = undefined
+    })
+
+    it('should compile', testLoader(
+        '<t-view>hello, world</t-view>',
+        function(err, code, map) {
+            expect(err).toBeFalsy();
+            expect(code).toBeTruthy();
+            expect(map).toBeTruthy();
+            expect(code).toMatchSnapshot();
+        }
+    ))
+})
+
+describe('h5', () => {
+    beforeAll(() => {
+        process.env.TARO_ENV = 'h5'
+    })
+
+    afterAll(() => {
+        process.env.TARO_ENV = undefined
+    })
+
+    it('should compile', testLoader(
+        '<t-view>hello, world</t-view>',
+        function(err, code, map) {
+            expect(err).toBeFalsy();
+            expect(code).toBeTruthy();
+            expect(map).toBeTruthy();
+            expect(code).toMatchSnapshot();
+        }
+    ))
+})
